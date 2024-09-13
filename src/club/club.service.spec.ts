5eea2ba0e049d674beedc5daeb6fc3a468f3e4e0
Nelson <n.ortizf@uniandes.edu.co>
2024-09-13 16:42:48 -0500

    pruebas logicas de club exitosas

--- src/club/club.service.spec.ts
+++ src/club/club.service.spec.ts
@@ -29,7 +29,7 @@ describe('ClubService', () => {
     for(let i = 0; i < 5; i++){
         const club: ClubEntity = await repository.save({
         nombre: faker.company.name(),                
-        fechafundacion: "2022/05/12",
+        fechafundacion: faker.date.anytime(),
         imagen: faker.image.url(),
         descripcion: faker.lorem.word(100), 
         socios: []
@@ -48,5 +48,75 @@ describe('ClubService', () => {
     expect(clubes).toHaveLength(clubesList.length);
   });
 
+  it('findOne should return a club by id', async () => {
+    const storedClub: ClubEntity = clubesList[0];
+    const club: ClubEntity = await service.findOne(storedClub.id);
+    expect(club).not.toBeNull();
+    expect(club.nombre).toEqual(storedClub.nombre)    
+    expect(club.fechafundacion).toEqual(storedClub.fechafundacion)
+    expect(club.imagen).toEqual(storedClub.imagen)
+    expect(club.descripcion).toEqual(storedClub.descripcion)    
+  });
+
+  it('findOne should throw an exception for an invalid club', async () => {
+    await expect(() => service.findOne("0")).rejects.toHaveProperty("message", "The club with the given id was not found")
+  });
+
+  it('create should return a new club', async () => {
+    const club: ClubEntity = {
+      id: "",
+      nombre: faker.company.name(),                
+      fechafundacion: faker.date.anytime(),
+      imagen: faker.image.url(),
+      descripcion: faker.lorem.word(100), 
+      socios: []
+    }
+
+    const newClub: ClubEntity = await service.create(club);
+    expect(newClub).not.toBeNull();
+
+    const storedClub: ClubEntity = await repository.findOne({where: {id: newClub.id}})
+    expect(storedClub).not.toBeNull();
+    expect(storedClub.nombre).toEqual(newClub.nombre)
+    expect(storedClub.descripcion).toEqual(newClub.descripcion)
+    expect(storedClub.fechafundacion).toEqual(newClub.fechafundacion)
+    expect(storedClub.imagen).toEqual(newClub.imagen)
+  });
+
+  it('update should modify a club', async () => {
+    const club: ClubEntity = clubesList[0];
+    club.nombre = "New name";
+    club.descripcion = "New description";
+  
+    const updatedClub: ClubEntity = await service.update(club.id, club);
+    expect(updatedClub).not.toBeNull();
+  
+    const storedClub: ClubEntity = await repository.findOne({ where: { id: club.id } })
+    expect(storedClub).not.toBeNull();
+    expect(storedClub.nombre).toEqual(club.nombre)
+    expect(storedClub.descripcion).toEqual(club.descripcion)
+  });
+
+  it('update should throw an exception for an invalid club', async () => {
+    let club: ClubEntity = clubesList[0];
+    club = {
+      ...club, nombre: "New name", descripcion: "New description"
+    }
+    await expect(() => service.update("0", club)).rejects.toHaveProperty("message", "The club with the given id was not found")
+  });
+
+  it('delete should remove a club', async () => {
+    const club: ClubEntity = clubesList[0];
+    await service.delete(club.id);
+  
+    const deletedClub: ClubEntity = await repository.findOne({ where: { id: club.id } })
+    expect(deletedClub).toBeNull();
+  });
+
+  it('delete should throw an exception for an invalid club', async () => {
+    const club: ClubEntity = clubesList[0];
+    await service.delete(club.id);
+    await expect(() => service.delete("0")).rejects.toHaveProperty("message", "The club with the given id was not found")
+  });
 
 });
