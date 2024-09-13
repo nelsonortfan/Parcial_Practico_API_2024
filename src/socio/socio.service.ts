b20405a0c809a09d5e2eee36331510af4bf6e362
Nelson <n.ortizf@uniandes.edu.co>
2024-09-13 15:17:50 -0500

    agregando services para club y socio

---
+++ src/socio/socio.service.ts
@@ -0,0 +1,51 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { SocioEntity } from './socio.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
+
+@Injectable()
+export class SocioService {
+
+    constructor(
+        @InjectRepository(SocioEntity)
+        private readonly socioRepository: Repository<SocioEntity>
+    ){}
+
+    async findAll(): Promise<SocioEntity []>{
+        return await this.socioRepository.find({ relations: ["clubes"]})
+    }
+
+    async findOne(id: string): Promise<SocioEntity>{
+        const socio : SocioEntity = await this.socioRepository.findOne({where: {id}, relations: ["clubes"]});
+
+        if (!socio)
+            throw new BusinessLogicException("The socio with the given id was not found", BusinessError.NOT_FOUND);
+
+        return socio;
+    }
+
+    async create(socio: SocioEntity): Promise<SocioEntity>{
+        return await this.socioRepository.save(socio);
+    }
+
+    async update(id: string, socio: SocioEntity): Promise<SocioEntity>{
+        const persistedSocio : SocioEntity = await this.socioRepository.findOne({where: {id}});
+        if(!persistedSocio)
+            throw new BusinessLogicException("The socio with the given id was not found", BusinessError.NOT_FOUND);
+
+        return await this.socioRepository.save({...persistedSocio, ...socio});
+    }
+
+    async delete(id: string){
+        const socio : SocioEntity = await this.socioRepository.findOne({where: {id}});
+        if(!socio)
+            throw new BusinessLogicException("The socio with the given id was not found", BusinessError.NOT_FOUND);
+
+        return await this.socioRepository.remove(socio);
+    }
+
+
+
+}
